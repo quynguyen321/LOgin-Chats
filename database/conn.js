6b68d655cdc0cfadbf08f8f73b068e98517e0e0e
quynguyen321 <quy.ybu@gmail.com>
2022-02-13 21:36:14 -0600

    logindone

---
+++ database/conn.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'login_sys'
+});
+
+con.connect((err) => {
+    if(err) throw err;
+    console.log('Database Connected..');
+});
+
+module.exports = con;
